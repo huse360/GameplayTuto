9208461a8b522fbc0089ff364a41975b1c0c931f
huse <huse@huseMBP.local>
2016-04-28 23:28:47 +0900

    Initial commit

---
+++ src/Main.cpp
@@ -0,0 +1,80 @@
+#include "Main.h"
+
+// Declare our game instance
+Main game;
+
+Main::Main()
+    : _scene(NULL), _wireframe(false)
+{
+}
+
+void Main::initialize()
+{
+    // Load game scene from file
+    _scene = Scene::load("res/demo.scene");
+
+    // Get the box model and initialize its material parameter values and bindings
+    Node* boxNode = _scene->findNode("box");
+    Model* boxModel = dynamic_cast<Model*>(boxNode->getDrawable());
+    Material* boxMaterial = boxModel->getMaterial();
+
+    // Set the aspect ratio for the scene's camera to match the current resolution
+    _scene->getActiveCamera()->setAspectRatio(getAspectRatio());
+}
+
+void Main::finalize()
+{
+    SAFE_RELEASE(_scene);
+}
+
+void Main::update(float elapsedTime)
+{
+    // Rotate model
+    _scene->findNode("box")->rotateY(MATH_DEG_TO_RAD((float)elapsedTime / 1000.0f * 180.0f));
+}
+
+void Main::render(float elapsedTime)
+{
+    // Clear the color and depth buffers
+    clear(CLEAR_COLOR_DEPTH, Vector4::zero(), 1.0f, 0);
+
+    // Visit all the nodes in the scene for drawing
+    _scene->visit(this, &Main::drawScene);
+}
+
+bool Main::drawScene(Node* node)
+{
+    // If the node visited contains a drawable object, draw it
+    Drawable* drawable = node->getDrawable(); 
+    if (drawable)
+        drawable->draw(_wireframe);
+
+    return true;
+}
+
+void Main::keyEvent(Keyboard::KeyEvent evt, int key)
+{
+    if (evt == Keyboard::KEY_PRESS)
+    {
+        switch (key)
+        {
+        case Keyboard::KEY_ESCAPE:
+            exit();
+            break;
+        }
+    }
+}
+
+void Main::touchEvent(Touch::TouchEvent evt, int x, int y, unsigned int contactIndex)
+{
+    switch (evt)
+    {
+    case Touch::TOUCH_PRESS:
+        _wireframe = !_wireframe;
+        break;
+    case Touch::TOUCH_RELEASE:
+        break;
+    case Touch::TOUCH_MOVE:
+        break;
+    };
+}
